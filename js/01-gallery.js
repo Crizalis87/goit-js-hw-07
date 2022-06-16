372793cec9b08e3a81c5f12829d585ff4ae43cbd
Crizalis87 <dmytroyermolayev@gmail.com>
2022-06-17 02:00:46 +0300

    update task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const photoGallery = document.querySelector(".gallery");
+
+const galleryBox = galleryItems.map(({ preview, original, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/>
+  </a>
+</div>`;
+});
+
+let modalBox;
+
+photoGallery.insertAdjacentHTML("afterbegin", galleryBox.join(""));
+
+photoGallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  modalBox = basicLightbox.create(
+    `<img src="${event.target.dataset.source}"/>`
+  );
+
+  modalBox.show();
+});
