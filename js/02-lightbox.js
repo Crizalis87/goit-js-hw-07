e69db677c63247518a9e8283c5240583a63abcce
Crizalis87 <dmytroyermolayev@gmail.com>
2022-06-17 23:12:43 +0300

    update task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const photoGallery = document.querySelector(".gallery");
+
+const galleryItem = galleryItems.map(({ preview, original, description }) => {
+  const galleryToReturn = `<a class = "gallery__link" href = "${original}">
+    <img 
+    class = "gallery__image"
+    src = "${preview}" 
+    data-source = "${original}" 
+    alt = "${description}"/></a>`;
+
+  return galleryToReturn;
+});
+
+photoGallery.insertAdjacentHTML("afterbegin", galleryItem.join(""));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
